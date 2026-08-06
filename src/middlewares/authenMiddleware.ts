8ae05eb93af7b5ce3fcfab2b50bcc202da735759
warat wongwichit <pleumcm18@gmail.com>
2026-08-06 11:10:25 +0700

    maleware

--- src/middlewares/authenMiddleware.ts
+++ src/middlewares/authenMiddleware.ts
@@ -10,6 +10,28 @@ export const authenticateToken = (
   res: Response,
   next: NextFunction
 ) => {
- 
-};
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
+  if (!token) {
+    return res.status(403).json({
+      success: false,
+      message: "Forbidden access",
+    });
+  }
+
+  const secret = process.env.JWT_SECRET || "your-secret-key";
+
+  jwt.verify(token, secret, function (err, decoded) {
+    if (err) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden access",
+      });
+    }
+
+    req.user = decoded as UserPayload;
+    req.token = token;
+    next();
+  });
+};
